aab83ed17b332437fd9375678bf1e9e21290e70c
Derek Volner <89471429+DerekVolner@users.noreply.github.com>
2023-11-30 01:12:43 -0600

    Add test for splitter function
    test checks that the splitter function is splitting on "." properly.

--- test_unittest.py
+++ test_unittest.py
@@ -13,3 +13,8 @@ class Test_TestMethods (unittest.TestCase):
         empty=[]
         BaseFileModProject6.Converter(empty,filledArray)
         self.assertEqual(empty,['122.','121a.','8.','99.','49.','5.','95.','3.','92.','88.','90.','95.'])
+    def test_splitter(self):
+        dottedArray = ["y.f.5.6.7.8.8.999.974.#.f"]
+        empty =[]
+        BaseFileModProject6.splitter(dottedArray, empty)
+        self.assertEqual(empty,["y","f","5","6","7","8","8","999","974","#","f"])
