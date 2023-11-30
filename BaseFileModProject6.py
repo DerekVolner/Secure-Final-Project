9d9fc8a98e2fe2a09ece4d494a2f1c490deeff44
Derek Volner <89471429+DerekVolner@users.noreply.github.com>
2023-11-30 03:30:01 -0600

    Apply PyLint reccomendations
    Applied indentation changes, unnecessary parentheses, Line too long, some trailing whitespaces.

--- BaseFileModProject6.py
+++ BaseFileModProject6.py
@@ -19,19 +19,19 @@ with anyone under any circumstances. '''
 def putter(base, read):
     for letter in base:
         read+=list(letter)  
-"""Converter appends the string value of the character to rest list after it's ascii value 
- is subtracted from it's index value in the read list"""
+#Converter appends the string value of the character to rest list after it's ascii value 
+#subtracted from it's index value in the read list"""
  
 def Converter(rest, read):
     for x in read:
-        if(x!="y"):
+        if x!="y":
             rest.append(str((ord(x))-read.index(x))+".") 
             read.remove(x)
             read.insert(0,'y') 
-        elif (x=="y"):
-             rest.append(str((ord(x))-read.index(x))+"a"+".")
-             read.remove(x)
-             read.insert(0,'y')
+        elif x=="y":
+            rest.append(str((ord(x))-read.index(x))+"a"+".")
+            read.remove(x)
+            read.insert(0,'y')
 def encrypt2(plaintext, encrypted):
     try:
         base_file=open(plaintext, "r",encoding="utf-8")
@@ -43,56 +43,56 @@ def encrypt2(plaintext, encrypted):
         Converter(rest, read)
         #writes the contents of the rest list to the output file
         for y in rest:
-                Encrypted_file.write(y)
-            #closes both output and input files and returns a print statement of the str and the value the user inputted for users_file_encryption at the start 
+            Encrypted_file.write(y)
+        #closes both output and input files and returns a print statement of the str and
+        # the value the user inputted for users_file_encryption at the start 
         base_file.close()
         Encrypted_file.close()
         return "Encrypted passwords wrote to "+ encrypted
-    except(FileNotFoundError):
+    except FileNotFoundError:
         return "The text file you specified cannot be found"
 #splits the encrypted file into single value strings
 def splitter(altered, eList):
     for letter in altered:
-         m=letter.split(".")
-         eList+=m
+        m=letter.split(".")
+        eList+=m
 #Takes the value of x and adds its index back to the value to return the character, 
 # then removes and replaces the index position with str (y)
 #and continues when x reaches ''    
 def process(eList, dList):
-      for x in eList:
+    for x in eList:
         y=eList.index(x)
-        if (x==''):
+        if x=='':
             continue
-        if(x=="121a"):
+        if x=="121a":
             z=int (121)
             dList.append(chr(z))
             eList.remove(x)
             eList.insert(0,'y')
-        elif(x!="121a"):
+        elif x!="121a":
             zy=int(x)
             dList.append(chr(((zy))+y))
             eList.remove(x)
             eList.insert(0,'y')
 def decrypt2(encrypted, decrypted):
     try:
-      altered_file=open(encrypted, "r",encoding="utf-8")
-      encryptedList =[]
-      decryptedList=[]
-      splitter(altered_file, encryptedList)
-      #above needed for splitter function
-      decrypted_file=open(decrypted,"w",encoding="utf-8")
-      process(encryptedList, decryptedList)
-      for character in decryptedList:
-        decrypted_file.write(character)
+        altered_file=open(encrypted, "r",encoding="utf-8")
+        encryptedList =[]
+        decryptedList=[]
+        splitter(altered_file, encryptedList)
+        decrypted_file=open(decrypted,"w",encoding="utf-8")
+        process(encryptedList, decryptedList)
+        for character in decryptedList:
+            decrypted_file.write(character)
 
-      altered_file.close()
-      decrypted_file.close()
-      return "Decrypted passwords wrote to "+ decrypted
-    except(ValueError):
+        altered_file.close()
+        decrypted_file.close()
+        return "Decrypted passwords wrote to "+ decrypted
+    except ValueError:
         return "The values in the file have not been encrypted with this program"
-    except(FileNotFoundError):
+    except FileNotFoundError:
         return "The text file you specified cannot be found"
-            
+#main             
 def main():
     print("Welcome to the password encryption program!")
     user_input=input("Options:\n< e for encryption >\n< d for decryption >\n< q to exit>\nSelect an option:")
