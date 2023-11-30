6db8593f05cf45986413bcb25a3a817d9ad260c1
Derek Volner <89471429+DerekVolner@users.noreply.github.com>
2023-11-30 01:21:37 -0600

    Add test for Process function
    Test passes an array of valid encrypted characters and checks if the process function is executing properly by having it write to an empty array and comparing it to what it should convert to.

--- test_unittest.py
+++ test_unittest.py
@@ -18,3 +18,8 @@ class Test_TestMethods (unittest.TestCase):
         empty =[]
         BaseFileModProject6.splitter(dottedArray, empty)
         self.assertEqual(empty,["y","f","5","6","7","8","8","999","974","#","f"])
+    def test_process(self):
+        encryptedArray =["121a","121", "8", "121a" , "121a", "121a" , "4", "115", "108" ,"121a"]
+        arrayDecrypted=[]
+        BaseFileModProject6.process(encryptedArray,arrayDecrypted)
+        self.assertEqual(arrayDecrypted,["y","z","\n", "y","y","y","\n", "z","t", "y" ] )
