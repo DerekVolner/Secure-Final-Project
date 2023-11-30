2db54c72076feb9ec62371d11408f89488e4266b
Derek Volner <89471429+DerekVolner@users.noreply.github.com>
2023-11-30 01:08:23 -0600

    Create Converter unittest
    test_Converter tests if Converter method is properly converting character values into ascii values and y into the special character 121a

--- test_unittest.py
+++ test_unittest.py
@@ -8,3 +8,8 @@ class Test_TestMethods (unittest.TestCase):
         read=[]
         BaseFileModProject6.putter(testArray,read)
         self.assertEqual(read, ["a","b","c"])
+    def test_Conveter(self):
+        filledArray=['z','y','\n','f','5','\n','e','\n','d','a','d','j']
+        empty=[]
+        BaseFileModProject6.Converter(empty,filledArray)
+        self.assertEqual(empty,['122.','121a.','8.','99.','49.','5.','95.','3.','92.','88.','90.','95.'])
